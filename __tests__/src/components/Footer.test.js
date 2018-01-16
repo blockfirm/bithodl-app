8f498647476a249377cf0044e06f1f00fd7c2495
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-01-16 23:46:10 +0300

    Test components/Footer.js

---
+++ __tests__/src/components/Footer.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Footer from '../../../src/components/Footer';
+
+describe('Footer', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <Footer />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
