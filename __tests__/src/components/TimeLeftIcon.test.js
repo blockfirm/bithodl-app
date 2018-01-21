03107916a5a5925c41f667e90822ae766bbfea31
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-01-21 17:44:31 +0300

    Test components/TimeLeftIcon.js with different time periods

--- __tests__/src/components/TimeLeftIcon.test.js
+++ __tests__/src/components/TimeLeftIcon.test.js
@@ -3,21 +3,38 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import TimeLeftIcon from '../../../src/components/TimeLeftIcon';
 
-describe('TimeLeftIcon', () => {
-  let fakeAddress;
+const RealDate = Date;
 
-  beforeEach(() => {
-    fakeAddress = {
-      id: 'cb3cd64d-4663-49a4-81f5-9a9985be58e9',
-      hash: '1JNHNJftiWu57UVpp2cjrNNHCeLwbNy3Zt',
-      script: 'OP_RETURN',
-      amount: 2000000,
-      createdAt: 1471651200,
-      unlockAt: 1482192000
-    };
+const mockDate = (fakeDate) => {
+  global.Date = class extends RealDate {
+    constructor(specifiedDate) {
+      super(...arguments);
+      return new RealDate(specifiedDate || fakeDate);
+    }
+  };
+
+  global.Date.now = jest.fn(() => new RealDate(fakeDate).getTime());
+};
+
+const unmockDate = () => {
+  global.Date = RealDate;
+};
+
+describe('TimeLeftIcon', () => {
+  beforeAll(() => {
+    mockDate('29 November, 2017, 21:59 GMT+02:00');
   });
 
-  it('renders correctly', () => {
+  afterAll(() => {
+    unmockDate();
+  });
+
+  it('renders correctly when there are years left', () => {
+    const fakeAddress = {
+      createdAt: 1471651200,
+      unlockAt: 1677578920
+    };
+
     const tree = renderer.create(
       <TimeLeftIcon start={fakeAddress.createdAt} end={fakeAddress.unlockAt} />
     ).toJSON();
@@ -25,7 +42,42 @@ describe('TimeLeftIcon', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  describe('#_getPeriod()', () => {
+  it('renders correctly when there are months left', () => {
+    const fakeAddress = {
+      createdAt: 1511639940,
+      unlockAt: 1519812520
+    };
 
+    const tree = renderer.create(
+      <TimeLeftIcon start={fakeAddress.createdAt} end={fakeAddress.unlockAt} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly when there are days left', () => {
+    const fakeAddress = {
+      createdAt: 1511639940,
+      unlockAt: 1513108740
+    };
+
+    const tree = renderer.create(
+      <TimeLeftIcon start={fakeAddress.createdAt} end={fakeAddress.unlockAt} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly when there is no time left', () => {
+    const fakeAddress = {
+      createdAt: 1471651200,
+      unlockAt: 1482192000
+    };
+
+    const tree = renderer.create(
+      <TimeLeftIcon start={fakeAddress.createdAt} end={fakeAddress.unlockAt} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
 });
